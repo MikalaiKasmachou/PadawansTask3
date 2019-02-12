bdfa0eac985579f010baf8ecaed79b333cc27850
Mikalai_Kasmachou <Mikalai_Kasmachou@epam.com>
2019-02-12 21:17:02 +0300

    additional tests

---
+++ Tests/StudentTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace PadawansTask3.Tests
+{
+    [TestFixture]
+    public class StudentTest
+    {
+        [Test]
+        public void TestCsAddition()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
